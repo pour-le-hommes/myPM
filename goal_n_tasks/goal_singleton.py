e7b30b79ec59bb0b15842779ee4929d033890eb5
pour-le-hommes <pourleterres@gmail.com>
2024-06-28 07:15:59 +0700

    Update eslint

--- goal_n_tasks/goal_singleton.py
+++ goal_n_tasks/goal_singleton.py
@@ -8,7 +8,7 @@ def get_goals(iteration:int):
         "x-vercel-protection-bypass":vercel_header
     }
 
-    resp = requests.get(f"{os.getenv("MY_BACKEND_WEBSITE")}/lifeup/tasks/{iteration}/sprint_goals", headers=header)
+    resp = requests.get(f"{os.getenv('MY_BACKEND_WEBSITE')}/lifeup/tasks/{iteration}/sprint_goals", headers=header)
 
     if resp.status_code == 200:
         # Decode the response content to a string
@@ -23,7 +23,7 @@ def get_tasks(iteration:int):
         "x-vercel-protection-bypass":vercel_header
     }
 
-    resp = requests.get(f"{os.getenv("MY_BACKEND_WEBSITE")}/lifeup/tasks/{iteration}/tasks", headers=header)
+    resp = requests.get(f"{os.getenv('MY_BACKEND_WEBSITE')}/lifeup/tasks/{iteration}/tasks", headers=header)
 
     if resp.status_code == 200:
         # Decode the response content to a string
